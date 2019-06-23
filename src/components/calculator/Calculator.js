a5cedff0abe9a038566d45fff570638c3bfbf33d
sam@sizzlecreative.co.uk <sam@sizzlecreative.co.uk>
2019-06-23 11:18:47 +0100

    First iteration of the simple calculator is complete

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -23,6 +23,7 @@ class Calculator extends Component {
         this._clearEverything = this._clearEverything.bind(this);
         this._clearLastWholeEntry = this._clearLastWholeEntry.bind(this);
         this._removeLastCharFromCalculation = this._removeLastCharFromCalculation.bind(this);
+        this._checkIfLastValueIsAMathOperator = this._checkIfLastValueIsAMathOperator.bind(this);
     }
 
     render() {
@@ -68,24 +69,42 @@ class Calculator extends Component {
      * @var {Any} value The value that is being pulled from the child component.
      */
     _captureKeyValue = (value) => {
+        // TODO: maybe change this to a switch statement.
         if (willClearAllCharacters(value)) return this._clearEverything();
         if (willClearLastWholeEntry(value)) return this._clearLastWholeEntry();
         if (willClearLastCharacter(value)) return this._removeLastCharFromCalculation();
-
         if (willPerformCalculation(value)) return this._doCalculation();
 
-        let shouldAddSpace = isMathematicalOperator(value) || willPerformCalculation(value);
-        let outputValue = isPercentage(value) ? ' / 100' : value;
+        if (this._checkIfLastValueIsAMathOperator(value) === false) {
+            let shouldAddSpace = isMathematicalOperator(value) || willPerformCalculation(value);
+            let outputValue = isPercentage(value) ? ' / 100' : value;
 
-        this.setState({calculation: shouldAddSpace ? this.state.calculation + " " + outputValue + " " : this.state.calculation + outputValue});
+            this.setState({calculation: shouldAddSpace ? this.state.calculation + ' ' + outputValue + ' ' : this.state.calculation + outputValue});
+        }
     }
 
     /**
      * This will parse the calculation and execute it.
      */
     _doCalculation = () => {
-        const result = eval(this.state.calculation);
-        this.setState({ result: result})
+        if(this.state.calculation !== '') {
+            let calculation = this.state.calculation;
+            let splitCalculation = calculation.split(" ");
+            let removeBlankEntries = splitCalculation.filter(arrValue => arrValue !== '');
+            let lastEntry = removeBlankEntries[removeBlankEntries.length - 1];
+
+            if (isMathematicalOperator(lastEntry)) {
+                removeBlankEntries.pop(lastEntry);
+                calculation = removeBlankEntries.join(" ");
+            }
+
+            const result = eval(calculation);
+            this.setState({ result: result.toFixed(2)});
+        }
+    }
+
+    _removeTrailingMathOperatorFromCalculation = () => {
+
     }
 
     /**
@@ -112,6 +131,23 @@ class Calculator extends Component {
     _removeLastCharFromCalculation = () => {
         const calculation = this.state.calculation;
         this.setState({calculation: calculation.substring(0, calculation.length - 1)});
+    }
+
+    /**
+     * This will check if the last entered value of a calculation is a key function as we
+     * don't want the user to enter things such as (45 ++ - 4);
+     */
+    _checkIfLastValueIsAMathOperator = (value) => {
+        const calculation = this.state.calculation;
+        let splitCalculation = calculation.split(" ");
+        let removeBlankEntries = splitCalculation.filter(arrValue => arrValue !== '');
+
+        let lastEntry = removeBlankEntries[removeBlankEntries.length - 1];
+
+        let isLastEntryMathOp = isMathematicalOperator(lastEntry) ? true : false;
+        let isCurrValueMathOp = isMathematicalOperator(value) ? true : false;
+
+        return isLastEntryMathOp === true && isCurrValueMathOp === true ? true : false;
     }
 
 }
