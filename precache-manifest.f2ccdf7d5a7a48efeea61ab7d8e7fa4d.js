35629156733b8cf513abf02a79732fa14784ac01
Sam <samogorm@gmail.com>
2019-07-09 09:59:07 +0100

    Updates

---
+++ precache-manifest.f2ccdf7d5a7a48efeea61ab7d8e7fa4d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b30be2c41cb6ce023b740d6b580a4f06",
+    "url": "/samogorm/simple-calculator/index.html"
+  },
+  {
+    "revision": "6e0f85fe9602badcfd34",
+    "url": "/samogorm/simple-calculator/static/css/main.3caffff7.chunk.css"
+  },
+  {
+    "revision": "48b4a4c27a025555fbe3",
+    "url": "/samogorm/simple-calculator/static/js/2.d352428b.chunk.js"
+  },
+  {
+    "revision": "6e0f85fe9602badcfd34",
+    "url": "/samogorm/simple-calculator/static/js/main.fdbd7fb6.chunk.js"
+  },
+  {
+    "revision": "a086fde88daffc852750",
+    "url": "/samogorm/simple-calculator/static/js/runtime~main.51894482.js"
+  },
+  {
+    "revision": "688422f58261d2f6076c8926819310bb",
+    "url": "/samogorm/simple-calculator/static/media/App-Icon.688422f5.svg"
+  },
+  {
+    "revision": "09f15129ba0f92f1045aa3b5a4fbc10e",
+    "url": "/samogorm/simple-calculator/static/media/divide.09f15129.svg"
+  },
+  {
+    "revision": "bf665f478d4d5fa833bd7cca4706d0c3",
+    "url": "/samogorm/simple-calculator/static/media/equals.bf665f47.svg"
+  },
+  {
+    "revision": "5252db72d8bd60c9a402bef392dd7567",
+    "url": "/samogorm/simple-calculator/static/media/minus.5252db72.svg"
+  },
+  {
+    "revision": "e5f57f20dbbf8ab65363c246e26d399e",
+    "url": "/samogorm/simple-calculator/static/media/plus.e5f57f20.svg"
+  },
+  {
+    "revision": "d5209bce302f9a726b5173b8823fa74b",
+    "url": "/samogorm/simple-calculator/static/media/times.d5209bce.svg"
+  }
+]);
